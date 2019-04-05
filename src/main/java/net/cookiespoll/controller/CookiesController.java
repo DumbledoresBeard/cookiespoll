04fa45c5e3036f0127630c7e80649a4f25ec01c7
Larisa Fedorchenko <lfedorchenko@lineate.com>
2019-04-05 21:57:18 +0600

    added controller and fto request for adding coookie

--- src/main/java/net/cookiespoll/controller/CookiesController.java
+++ src/main/java/net/cookiespoll/controller/CookiesController.java
@@ -1,4 +1,23 @@
 package net.cookiespoll.controller;
 
-public class cookiesController {
+import net.cookiespoll.dto.AddCookieDtoRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+@Controller
+public class CookiesController {
+
+    @RequestMapping(value = "/addcookie",
+            method = RequestMethod.POST)
+    @ResponseBody
+    public String addCookie(
+            @Valid @ModelAttribute AddCookieDtoRequest addCookieDtoRequest,
+            HttpServletResponse response) {
+
+        return "file has been uploaded";
+
+    }
 }
--- src/main/java/net/cookiespoll/dto/AddCookieDtoRequest.java
+++ src/main/java/net/cookiespoll/dto/AddCookieDtoRequest.java
@@ -1,4 +1,48 @@
-package net.cookiespoll.controller.net.cookiespoll;
+package net.cookiespoll.dto;
 
-public class dto {
+import org.springframework.web.multipart.MultipartFile;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
+
+
+public class AddCookieDtoRequest {
+
+    @NotNull(message = "Cookie name cannot be null")
+    @Size(min = 4, max = 30, message = "Cookie name must be between 4 and 30 characters")
+    private String name;
+
+    @NotNull(message = "Cookie description cannot be null")
+    @Size(max = 150, message = "Cookie description must be less then 150 characters")
+    private String description;
+
+    @NotNull(message = "Cookie photo cannot be null")
+    private MultipartFile file;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
 }
