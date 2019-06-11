5eaafb6049ff624aac5b3a3ae7c2bcf7e1642c5e
Larisa Fedorchenko <lfedorchenko@ondeck.com>
2019-06-11 22:07:34 +0600

    added tests for OAuth2AuthenticationSuccessHandler

--- src/test/java/net/cookiespoll/auth/TestHttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/test/java/net/cookiespoll/auth/TestHttpCookieOAuth2AuthorizationRequestRepository.java
@@ -17,9 +17,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
-import static org.powermock.api.mockito.PowerMockito.verifyStatic;
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(CookieWebUtils.class)
@@ -28,8 +29,12 @@ public class TestHttpCookieOAuth2AuthorizationRequestRepository {
     private HttpServletRequest request = mock(HttpServletRequest.class);
     private CookieWebUtils cookieWebUtils = mock(CookieWebUtils.class);
     private HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository = new HttpCookieOAuth2AuthorizationRequestRepository();
-    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
     private Cookie cookieOAuth2 = new Cookie("oauth2_auth_request", "value");
+    private OAuth2AuthorizationRequest requestOAuth2 = OAuth2AuthorizationRequest.authorizationCode().authorizationUri("234").clientId("12").build();
+
+    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
+    private static final int cookieExpireSeconds = 180;
 
     @Before
     public void init() {
@@ -39,7 +44,6 @@ public class TestHttpCookieOAuth2AuthorizationRequestRepository {
     @Test
     public void testLoadAuthorizationRequest () {
         PowerMockito.mockStatic(CookieWebUtils.class);
-        OAuth2AuthorizationRequest requestOAuth2 = OAuth2AuthorizationRequest.authorizationCode().authorizationUri("234").clientId("12").build();
 
         when(CookieWebUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME))
                 .thenReturn(Optional.of(cookieOAuth2));
@@ -48,5 +52,23 @@ public class TestHttpCookieOAuth2AuthorizationRequestRepository {
         OAuth2AuthorizationRequest resultOAuth2 = httpCookieOAuth2AuthorizationRequestRepository.loadAuthorizationRequest(request);
 
         Assert.assertEquals(resultOAuth2, requestOAuth2);
+    }
+
+    @Test
+    public void testSaveAuthorizationRequestOAuth2AuthorizationRequestNotNull () throws Exception {
+//        PowerMockito.mockStatic(CookieWebUtils.class);
+//
+//        when(CookieWebUtils.serialize(requestOAuth2)).thenReturn("serialized request");
+//        doNothing().when(CookieWebUtils.class, "addCookie", response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME,
+//                CookieWebUtils.serialize(requestOAuth2), cookieExpireSeconds);
+        when(request.getParameter(REDIRECT_URI_PARAM_COOKIE_NAME)).thenReturn("redirect_uri_after_login");
+//        doNothing().when(CookieWebUtils.class, "addCookie", response, REDIRECT_URI_PARAM_COOKIE_NAME, "redirect_uri_after_login",
+//                cookieExpireSeconds);
+
+        httpCookieOAuth2AuthorizationRequestRepository.saveAuthorizationRequest(requestOAuth2, request, response);
+
+//        verify(response).addCookie(any(Cookie.class));
+//        verify(request).getParameter(REDIRECT_URI_PARAM_COOKIE_NAME);
+//        PowerMockito.verifyStatic(CookieWebUtils);
     }
 }
---
+++ src/test/java/net/cookiespoll/auth/TestOAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,61 @@
+package net.cookiespoll.auth;
+
+import net.cookiespoll.utils.CookieWebUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+public class TestOAuth2AuthenticationSuccessHandler {
+    private HttpServletResponse response = mock(HttpServletResponse.class);
+    private HttpServletRequest request = mock(HttpServletRequest.class);
+    private TokenProvider tokenProvider = mock(TokenProvider.class);
+    private Authentication authentication = mock(Authentication.class);
+    private HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository =
+            mock(HttpCookieOAuth2AuthorizationRequestRepository.class);
+    private Cookie cookieOAuth2 = new Cookie("oauth2_auth_request", "value");
+    private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+    public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
+
+    @Before
+    public void init() {
+        oAuth2AuthenticationSuccessHandler = new OAuth2AuthenticationSuccessHandler(tokenProvider,
+                httpCookieOAuth2AuthorizationRequestRepository);
+
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testOnAuthenticationSuccess () throws IOException {
+        Cookie cookie = new Cookie("redirect_uri", "value");
+        Cookie[] cookies = {cookie, new Cookie("cookie2", "value2")};
+
+        when(request.getCookies()).thenReturn(cookies);
+        when(tokenProvider.createToken(authentication)).thenReturn("token");
+        doNothing().when(httpCookieOAuth2AuthorizationRequestRepository).removeAuthorizationRequestCookies(request, response);
+        when(response.isCommitted()).thenReturn(false);
+
+        oAuth2AuthenticationSuccessHandler.onAuthenticationSuccess(request, response, authentication);
+
+        verify(request).getCookies();
+        verify(tokenProvider).createToken(authentication);
+        verify(httpCookieOAuth2AuthorizationRequestRepository).removeAuthorizationRequestCookies(request, response);
+        verify(response).isCommitted();
+    }
+}
